e6337c2a647d60c02c22eb8c947c9f699aa0f834
necromanteion <wmmedlar@gmail.com>
2014-12-09 14:09:04 -0600

    Update fetch.js
    featuring argument support!

--- fetch.js
+++ fetch.js
@@ -1,42 +1,57 @@
 module.exports = function(self) {
+	var fs = require("fs");
+		XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 	return {
 		"actionName": "fetch",
 		"actionTrigger": new RegExp("^.fetch\\b", "i"),
 		"actionFunction": function fetch(nick, chan, arguments) {
-			fs = require("fs");
-			XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-			
-			xhr = []
-			api = ["awakenings"]  //["active_skills","awakenings","evolutions","leader_skills","monsters"];
-			url = []
-			
-			for (i = 0; i < api.length; i++) {
-				(function (i) {
-					xhr[i] = new XMLHttpRequest();
-					url[i] = "https://www.padherder.com/api/" + api[i] + "/"
-					xhr[i].open("GET", url[i], true);
-					xhr[i].onreadystatechange = function() {
-						if (xhr[i].readyState == 4 && xhr[i].status == 200) {
-							response = JSON.parse(xhr[i].responseText);
-								data = [];
-							for (j = 0; j < response.length; j++) {
-								if (response[j].id) {
-									data[response[j].id] = response[j];
-								} else {
-									data = response;
-									break;
-								};
-							};
-							fs.writeFile(api[i] + ".js", JSON.stringify(data), function(err) {
-								if (err) throw err;
-								//console.log(api[i] + " saved to file!");
-							});
+			var arguments = arguments.split(" ");
+				api = ["active_skills","awakenings","evolutions","leader_skills","monsters"]
+				target = []
+				if (arguments == undefined) {
+					target = api;
+				} else {				
+					for (i = 0; i < arguments.length; i++) {
+						if (api.indexOf(arguments[i])) {
+							target.push(api[api.indexOf(arguments[i])]);
 						};
 					};
-					xhr[i].send();
-				})(i);
-			};
-			return "All data saved to file!";
+				};
+				
+				var xhr = []
+					url = []
+				
+				if (target.length > 0) {
+					for (i = 0; i < target.length; i++) {
+						(function (i) {
+							xhr[i] = new XMLHttpRequest();
+							url[i] = "https://www.padherder.com/api/" + target[i] + "/"
+							xhr[i].open("GET", url[i], true);
+							xhr[i].onreadystatechange = function() {
+								if (xhr[i].readyState == 4 && xhr[i].status == 200) {
+									response = JSON.parse(xhr[i].responseText);
+										data = [];
+									for (j = 0; j < response.length; j++) {
+										if (response[j].id) {
+											data[response[j].id] = response[j];
+										} else {
+											data = response;
+											break;
+										};
+									};
+									fs.writeFile(target[i] + ".js", JSON.stringify(data), function(err) {
+										if (err) throw err;
+										//console.log(target[i] + " saved to file!");
+									});
+								};
+							};
+							xhr[i].send();
+						})(i);
+					};
+					return "All data saved to file!";
+				} else {
+					return "Onii-chan check your spelling!";
+				};
 		},
 		"responseMethods": {"pm":"pm","public":"pm"}
 	};
