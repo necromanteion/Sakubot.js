8971be968dc1f3721cab07279b17d881160553f8
necromanteion <wmmedlar@gmail.com>
2014-12-10 14:09:12 -0600

    Update rem.js

--- scripts/rem.js
+++ scripts/rem.js
@@ -4,18 +4,35 @@ module.exports = function() {
 	return {
 		"name": "REM",
 		"trigger": new RegExp("^.rem\\b", "i"),
+		"exclude": ["Godzilla", "Bastet"],
 		"action": function (arguments, nick, target) {
-			if (nick === target) {
-				var nick = "Your ";
-			} else {
-				var nick = nick + "\'s";
+			if (arguments) {
+				var arguments = arguments.split(" ");
+				for (i = arguments.length - 1; i >= 0; i--) {
+					if (isFinite(arguments[i])) {
+						var n = parseInt(arguments[i]);
+						arguments.splice(1,1);
+						break;
+					};
+				};
 			};
+			
+			if (n <= 0 || n == undefined) { var n = 1; }
+			else if (n > 5 && target.charAt(0) == "#") { return "Five rolls maximum please!"; }
+			else if (n > 10 && target.charAt(0) != "#") { return "Ten rolls maximum please!"; };
+			
+			if (target.charAt(0) == "#") { var target = nick + "\'s "; }
+			else { var target = "Your "; };
+			
 			fs.readFile("./data/monsters.json", {encoding: "utf8"}, function (err, data) {
-				if (err) throw err;
-				console.log(typeof JSON.parse(data));
-				console.log(JSON.parse(data)[752]);
+				if (err) return "I couldn't find the monster data!";
+				var response = []
+				for (i = 0; i < n; i++) {
+					response.push(target + "REM roll: " + JSON.parse(data)[rem[Math.floor(Math.random() * rem.length)]].name + "!");
+				};
+				var response = response.join("\n)").toString();
 			});
-			return ".rem!";
+			return response;
 		}
 	};
 };
