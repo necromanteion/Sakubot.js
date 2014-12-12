0e48e802286fda6e98fb7eee5ec983d754d97944
necromanteion <wmmedlar@gmail.com>
2014-12-12 06:19:16 -0600

    Update event.js

--- scripts/event.js
+++ scripts/event.js
@@ -1,6 +1,6 @@
 module.exports = function() {
-	var fs = require("fs");
-		XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	//var fs = require("fs");
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 	return {
 		"name": "Events",
 		"trigger": new RegExp("^.events?\\b", "i"),
@@ -8,18 +8,40 @@ module.exports = function() {
 		"action": function(arguments) {
 			var xhr = new XMLHttpRequest();
 				url = "https://www.padherder.com/api/events/";
+				events = [];
 				
-			xhr.onreadystatechange = function() {
-				console.log("readyState: " + xhr.readyState);
-				console.log("status: " + xhr.status);
-				if (xhr.readyState == 4 && xhr.status == 200) {
-					console.log(xhr.responseText);
-					var response = JSON.parse(xhr.responseText);
-				};
+			if (xhr != null) {
+				xhr.open("GET", url, false);
+				xhr.send();
+				events = JSON.parse(xhr.responseText);
 			};
-			xhr.open("GET", url, true);
-			xhr.send();
-			console.log(response);
+				
+			if (arguments) {
+				var arguments = arguments.split(" ");
+				for (i = 0; i < arguments.length; i++) {
+					if (isFinite(arguments[i])) {
+						var n = parseInt(arguments[i]);
+						arguments.splice(i,1);
+						break;
+					} else if (arguments[i] === "all") {
+						var n = events.length
+						arguments.splice(i,1);
+						break;
+					};
+				};
+			} else { var arguments = ["A","B","C","D","E","1","2","3"];	};
+			
+			var groups = arguments.toUpperCase().slice(0);
+			console.log(groups);
+			
+			if (n <= 0 || n == undefined) { var n = 2; }
+			else if (n > 5 && target.charAt(0) == "#") { var n = 4; }
+			//else if (n > 10 && target.charAt(0) != "#") { var n = 10; };
+
+			events = events.filter(function(value, index) {
+				return groups.indexOf(events[index].group_name) >= 0;
+			});
+			console.log(events);
 			return "okay";
 		}
 	};
