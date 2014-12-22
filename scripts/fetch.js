09527dbb80f70b4d81d10f9a6a562640d5134257
necromanteion <wmmedlar@gmail.com>
2014-12-22 14:58:26 -0600

    Update fetch.js

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -2,62 +2,70 @@ module.exports = function() {
 	var fs = require("fs");
 		XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 	return {
-		"name": "fetch",
-		"trigger": new RegExp("^.fetch\\b", "i"),
+		"name": "Fetch",
+		"trigger": new RegExp("^\\.fetch\\b", "i"),
 		"restrict": ["necromanteion"],
-		"action": function (arguments) {
+		"actionType": "privmsg",
+		"action": function (message, nickname, target) {
 			var	api = ["active_skills","awakenings","evolutions","leader_skills","monsters"];
-				target = [];
-				arguments = arguments.trim();
-			if (!arguments) {
-				return "Fetch what, onii-chan?";
-			} else if (arguments === "all") {
-				Array.prototype.push.apply(target, api);
+				apiGET = [];
+				message = message.trim();
+			if (!message) {
+				return [target,"Fetch what, onii-chan?"];
+			} else if (message === "all") {
+				Array.prototype.push.apply(apiGET, api);
 			} else {
-				var arguments = arguments.split(" ");
+				var message = message.split(" ");
 			};
 			
-			for (i = 0; i < arguments.length; i++) {
-				if (api.indexOf(arguments[i]) >= 0) {
-					target.push(api[api.indexOf(arguments[i])]);
+			for (i = 0; i < message.length; i++) {
+				if (api.indexOf(message[i]) >= 0) {
+					apiGET.push(api[api.indexOf(message[i])]);
 				};
 			};
 			
 			var xhr = [];
 				url = [];
 				
-			if (target.length > 0) {
-				for (i = 0; i < target.length; i++) {
+			if (apiGET.length > 0) {
+				for (i = 0; i < apiGET.length; i++) {
 					(function (i) {
 						xhr[i] = new XMLHttpRequest();
-						url[i] = "https://www.padherder.com/api/" + target[i] + "/"
+						url[i] = "https://www.padherder.com/api/" + apiGET[i] + "/"
 						xhr[i].open("GET", url[i], true);
 						xhr[i].onreadystatechange = function() {
 							if (xhr[i].readyState == 4 && xhr[i].status == 200) {
 								response = JSON.parse(xhr[i].responseText);
-									data = [{"empty":"empty"}] ;
-								for (j = 0; j < response.length; j++) {
-									if (response[j].id) {
+									data = {};
+								
+								length = (Object.keys(response).length ? Object.keys(response).length : response.length);
+
+								for (j = 0; j < length; j++) {
+									if (apiGET[i] == "evolutions") {
+										data[j] = [];
+										for (k = 0; k < response[Object.keys(response)[j]].length; k++) {
+											data[j][k] = response[Object.keys(response)[j]][k];
+										};
+									} else if (response[j].id) {
 										data[response[j].id] = response[j];
-									} else {
-										data = response;
-										break;
-									};
+									} else if (response[j].name) {
+										data[response[j].name] = response[j];
+									} else { return [target,"Onii-chan I ran into an error fetching " + apiGET[i] + "!"]; };
 								};
-								fs.writeFile("./data/" + target[i] + ".json", JSON.stringify(data), function(err) {
+
+								fs.writeFile("./data/" + apiGET[i] + ".json", JSON.stringify(data), function(err) {
 									if (err) fs.mkdir("./data/", function() { });
-									//console.log(target[i] + " saved to file!");
 								});
 							};
 						};
 						xhr[i].send();
 					})(i);
 				};
-				fileResponse = target.slice(0);
+				fileResponse = apiGET.slice(0);
 				if (fileResponse.length > 1) { fileResponse[fileResponse.length - 1] = "and " + fileResponse[fileResponse.length - 1]; };
-				return "Fetching " + fileResponse.join(", ") + " from padherder!";
+				return [target,"Fetching " + fileResponse.join(", ") + " from padherder!"];
 			} else {
-				return "Onii-chan check your spelling!";
+				return [target,"Onii-chan check your spelling!"];
 			};
 		},
 	};
