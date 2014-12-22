a59d01ff6efd20d2ee8bef06f2483fc2de9ea660
necromanteion <wmmedlar@gmail.com>
2014-12-22 14:55:12 -0600

    Update Sakubot.js
    support for non-message events (e.g., join, part)

--- Sakubot.js
+++ Sakubot.js
@@ -17,7 +17,7 @@ var Sakubot = new factory.Api();
 	client = Sakubot.createClient("Sakubot", config);
 
 Sakubot.hookEvent("Sakubot", "notice", function(message) {
-	//console.log(message.message)
+	console.log(message.message)
 });
 
 Sakubot.hookEvent("Sakubot", "registered", function(message) {
@@ -30,15 +30,11 @@ Sakubot.hookEvent("Sakubot", "registered", function(message) {
 });
 
 Sakubot.hookEvent("Sakubot", "privmsg", function(message) {
-	//console.log(message);
 	for (i = 0; i < actions.length; i++) {
 		if (message.message.match(actions[i].trigger) && !message.nickname.match(actions[i].exclude)) {
-			if (message.target.charAt(0) == "#") { var target = message.target; }
-			else { var target = message.nickname; };
-			
-			var arguments = message.message.replace(actions[i].trigger, "").trim();
-				message = actions[i].action(arguments, message.nickname, message.target);
-			client.irc.privmsg(target, message);
+			date = new Date();
+			console.log("[" + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2) + "] <" + message.nickname + "> " + message.message );
+			handleResponse(actions[i].actionType, actions[i].action(message.message.replace(actions[i].trigger, "").trim(), message.nickname, message.target));
 			break;
 		};
 	};
@@ -51,19 +47,25 @@ Sakubot.hookEvent("Sakubot", "privmsg", function(message) {
 	for (i = 0; i < scripts.length; i++) {
 		if (scripts[i].indexOf(".js") < 0) { continue; };
 		script = require("./scripts/" + scripts[i])();
-		if (typeof script.trigger === undefined || typeof script.action === undefined) { continue; };
-		actions.push(script);
+		if (script.length) {
+			for (j = 0; j < script.length; j++) {
+				actions.push(script[j]);
+			};
+		} else { actions.push(script); };
 	};
-	
+
 	actions.push({
 		"name": "(Re)Load",
-		"trigger": new RegExp("^.(re)?load\\b", "i"),
+		"trigger": new RegExp("^\\.(re)?load\\b", "i"),
 		"restrict": ["necromanteion"],
-		"action": function() {
+		"actionType": "privmsg",
+		"action": function(message, nickname, target) {
+			target = (target.charAt(0) == "#" ? target : nickname);
+		
 			var path = fs.realpathSync("./scripts/") + "\\";
 				scripts = fs.readdirSync("./scripts/");
 			for (i = 0; i < scripts.length; i++) { delete require.cache[path + scripts[i]];	};
-			return load();
+			return [target,load()];
 		}
 	});
 	
@@ -73,3 +75,36 @@ Sakubot.hookEvent("Sakubot", "privmsg", function(message) {
 	};
 	return config.nick + " rebooted and ready to rock!";
 })();
+
+function handleResponse(actionType, response) {
+	pickAction = ["privmsg","notice","join","part","mode","topic","me","ctcp","disconnect","reconnect"];
+	d = new Date();
+			//console.log("[" + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2) + "] <" + message.nickname + "> " + message.message );
+	doAction = [
+		function(response) {
+			client.irc.privmsg(response[0],response[1]);			//target, message
+			console.log("[" + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2) + "] <" + config.nick + "> " + response[1] );
+			},
+		function(response) { client.irc.notice(response[0],response[1]); },				//target, message
+		function(response) {
+			client.irc.join(response[0],response[1]);				//channel, [password]
+			console.log("[" + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2) + "] * " + config.nick + " has joined " + response[0]);
+			},
+		function(response) {
+			client.irc.part(response[0],response[1]);				//channel, [message]
+			console.log("[" + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2) + "] * " + config.nick + " has left " + response[0] + (response[1].length > 0 ? " (" + response[1] + ")" : "") );
+			},
+		function(response) { client.irc.mode(response[0],response[1]); },				//target, mode
+		function(response) { client.irc.topic(response[0],response[1]); },				//channel, topic
+		function(response) {
+			client.irc.me(response[0],response[1]);					//target, message
+			console.log("[" + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2) + "] * " + config.nick + response[1] );
+			},
+		function(response) { client.irc.ctcp(response[0],response[1],response[2]); },	//target, type, message
+		function(response) { client.irc.disconect(response[0]); },						//message
+		function(response) { client.irc.reconnect(); }
+	];
+	
+	doAction[pickAction.indexOf(actionType)](response);
+	return;
+};
